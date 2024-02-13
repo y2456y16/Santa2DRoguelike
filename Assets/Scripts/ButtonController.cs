1d04b6bb8327c1e2d339a216481c5c88b5d7806f
jihye <thoth_0302@naver.com>
2024-02-13 11:20:53 +0900

    <feat> scene 이동

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
     [SerializeField] private GameObject optionUI;
+    [SerializeField] private GameObject statusUI;
 
+
+
+    //UI
     public void OnOptionUIButton()
     {
         optionUI.SetActive(true);
@@ -14,5 +19,12 @@ public class ButtonController : MonoBehaviour
     public void OffOptionUIButton()
     {
         optionUI.SetActive(false);
+    }
+
+
+    //SceneLoad
+    public void MainSceneLoad()
+    {
+        SceneManager.LoadScene("TestMainScene");
     }
 }
