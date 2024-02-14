ac7a0e87599a8f6353dfde59a1a80c7a5daa9dfd
jihye <thoth_0302@naver.com>
2024-02-14 18:38:44 +0900

    <fix> UI text와 player stats 연결

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -10,17 +10,29 @@ public class GameManager : MonoBehaviour
     public Transform Player { get; private set; }
     [SerializeField] private string playerTag = "Player";
 
-    public List<Vector3> enemyLocation = new List<Vector3>();//evemy start location list
+    public List<Vector3> enemyLocation = new List<Vector3>();//enemy start location list
                                                              // Start is called before the first frame update
+
+
+    private CharacterStatsHandler characterStats;
+    [HideInInspector] public int player_health;
+    [HideInInspector] public float player_speed;
+    [HideInInspector] public int player_atk;
+    [HideInInspector] public int player_def;
+    [HideInInspector] public StatsChangeType player_type;
+
 
     private void Awake()
     {
         Instance = this;
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;//gets transform data of object(tag == player)
+        characterStats = Player.GetComponent<CharacterStatsHandler>();
         EnemyLocationSet();
+        
     }
     void Start()
     {
+        SetPlayerStats();
         Time.timeScale = 1f;
         InvokeRepeating("EnemyCreate", 0.5f, 2f);
     }
@@ -43,5 +55,13 @@ public class GameManager : MonoBehaviour
         enemyLocation.Add(new Vector3(3f, 0f, 0));
         enemyLocation.Add(new Vector3(-3f, -4f, 0));
         enemyLocation.Add(new Vector3(3f, -4f, 0));
+    }
+
+    void SetPlayerStats()
+    {
+        player_health = characterStats.CurrentStats.maxHealth;
+        player_atk = characterStats.CurrentStats.atk;
+        player_def = characterStats.CurrentStats.def;
+        player_speed = characterStats.CurrentStats.speed;
     }
 }
