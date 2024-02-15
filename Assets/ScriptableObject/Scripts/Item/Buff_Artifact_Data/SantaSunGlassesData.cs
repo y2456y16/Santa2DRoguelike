e44e81c7b650a92a40ba53f3859214c8471bd15e
gunwonpark <parkgunwon1@naver.com>
2024-02-15 20:28:35 +0900

    <feat> 선글라스 아이템 능력 추가

--- Assets/ScriptableObject/Scripts/Item/Buff_Artifact_Data/SantaSunGlassesData.cs
+++ Assets/ScriptableObject/Scripts/Item/Buff_Artifact_Data/SantaSunGlassesData.cs
@@ -6,12 +6,23 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "ItemSO/SantaSunGlassesData")]
 public class SantaSunGlassesData : ItemSO
 {
+    public int Attack = 1;
+    public int Defense = 1;
+    public float Speed = 1f;
     SantaSunGlassesData()
     {
         Name = "SantaSunGlasses";
     }
     public override void ApplyEffect(GameObject target)
     {
-        // TODO 모든 맵 밝히기
+        // TODO 공격력 방어력 스피드 1씩 올리기
+        CharacterStatsHandler playerStat = target.GetComponent<CharacterStatsHandler>();
+        CharacterStats newStat = new CharacterStats();
+        newStat.statsChangeType = StatsChangeType.Add;
+        newStat.speed = Speed;
+        newStat.atk = Attack;
+        newStat.def = Defense;
+        playerStat.AddStatModifier(newStat);
+        UIManager.Instance.SetStatsText();
     }
 }
