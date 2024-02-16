f5ba15eff3f37e2ff9d71c6a431c0bf44a69dfdc
y2456y16 <y2456y16@gmail.com>
2024-02-16 10:02:18 +0900

    <fix> option button delete

--- Assets/Scripts/UI/ButtonController.cs
+++ Assets/Scripts/UI/ButtonController.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class ButtonController : MonoBehaviour
 {
-    [SerializeField] private GameObject optionUI;
     [SerializeField] private GameObject statusUI;
     [SerializeField] private GameObject status_optionUI;
     private GameObject storyUI;
@@ -23,28 +22,14 @@ public class ButtonController : MonoBehaviour
     }
 
     //IntroScene ------------------------------------------------------------------------
-    //UI
-    public void OnOptionUIButton()
-    {
-        statusUI.SetActive(false);
-        optionUI.SetActive(true);
-    }
-
-    public void OffOptionUIButton()
-    {
-        optionUI.SetActive(false);
-    }
 
     //SceneLoad
     public void MainSceneLoad()
     {
         storyUI = Resources.Load<GameObject>("Prifabs/Story_Canvas");
         //설정UI가 active true 상태이면, 버튼을 눌러도 실행되지 않도록 설정
-        if (!optionUI.activeInHierarchy)
-        {
-            gameObject.SetActive(false);
-            Instantiate(storyUI);
-        }
+        gameObject.SetActive(false);
+        Instantiate(storyUI);
     }
 
     //MainScene ------------------------------------------------------------------------
@@ -54,14 +39,12 @@ public class ButtonController : MonoBehaviour
         Time.timeScale = 0f;
         status_optionUI.SetActive(true);
         statusUI.SetActive(true);
-        optionUI.SetActive(false);
     }
 
     public void OffStatusUIButton()
     {
         status_optionUI.SetActive(false);
         statusUI.SetActive(true);
-        optionUI.SetActive(false);
         Time.timeScale = 1f;
     }
 }
